9371e272963ef4feba24d2f156e264e171db01e7
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-04-24 22:46:11 +0300

    hw11

--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -1,9 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    navText: ['←', '→'],
+    overlayOpacity: 0.9,
+    animationSpeed: 200,
+    doubleTapZoom: 1.5,
+});
+
 const gallery = document.querySelector('.gallery');
+
 export const renderGallery = (images) => {
     images.forEach((image) => {
         const photoCard = document.createElement('div');
         photoCard.classList.add("photo-card");
-        photoCard.innerHTML = `
+      photoCard.innerHTML = `
+        <a class="gallery__link" href="${image.largeImageURL}">
         <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
             <div class="info">
                 <p class="info-item">
@@ -18,8 +32,8 @@ export const renderGallery = (images) => {
                 <p class="info-item">
                   <b>Downloads</b>${image.downloads}
                 </p>
-            </div>`;
-        
+            </div>
+        </a>`;
         gallery.appendChild(photoCard);
     });
 };
