ac297095bef1419a3baad3396e8c81bdc9bd4f21
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-04-26 21:08:31 +0300

    hw11

--- src/js/index.js
+++ src/js/index.js
@@ -18,6 +18,7 @@ const loadMoreBtn = document.querySelector('.load-more');
 
 let currentPage = 1;
 let searchQuery = "";
+let isLoading = false;
 
 form.addEventListener('submit', handleFormSubmit);
 window.addEventListener('scroll', handleScroll);
@@ -29,34 +30,44 @@ async function handleFormSubmit(event) {
     searchQuery = event.target.elements.searchQuery.value.trim();
     currentPage = 1;
     clearGallery(gallery);
-    try {
-        const { hits, totalHits } = await fetchImages(searchQuery, currentPage);
-        if (hits.length === 0) {
-            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            loadMoreBtn.classList.add('hidden-button');
-            return;
+    console.log(searchQuery !== "")
+    if (searchQuery === "") {
+        clearGallery(gallery);
+        loadMoreBtn.classList.add('hidden-button');
+        return;
+    } else {
+        try {
+            const { hits, totalHits } = await fetchImages(searchQuery, currentPage);
+            if (hits.length === 0) {
+                Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+                loadMoreBtn.classList.add('hidden-button');
+                return;
+            }
+            renderGallery(hits);
+            loadMoreBtn.classList.remove('hidden-button');
+            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+        } catch (error) {
+            console.error(error);
         }
-        renderGallery(hits);
-        loadMoreBtn.classList.remove('hidden-button');
-        Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
-    } catch (error) {
-        console.error(error);
     }
 };
 
-async function handleScroll() {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight - 10) {
-        loadMoreBtn.click();
-    }
-}
+// async function handleScroll() {
+//     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+//     if (scrollTop + clientHeight >= scrollHeight - 10) {
+//         loadMoreBtn.click();
+//         return;
+//     }
+// }
 
 async function handleLoadMore() {
+    isLoading = true;
     loadMoreBtn.classList.add('hidden-button');
     currentPage++;
     try {
         const { hits, totalHits } = await fetchImages(searchQuery, currentPage);
-        if (totalHits < currentPage * 40 ) {
+        if (totalHits < currentPage * 40) {
+            isLoading = true;
             Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
             loadMoreBtn.classList.add('hidden-button');
             return;
@@ -65,5 +76,19 @@ async function handleLoadMore() {
         loadMoreBtn.classList.remove('hidden-button');
     } catch (error) {
         console.error(error);
+    } finally {
+        isLoading = false;
     }
 };
+
+async function handleScroll() {
+    if (isLoading) {
+        return;
+    }
+
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
+        handleLoadMore();
+    }
+}
